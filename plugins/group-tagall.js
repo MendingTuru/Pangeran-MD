a757e0595bc9c39651029789460bb2c6328ecacf
Kri.com <107316046+KrizynOfc@users.noreply.github.com>
2022-06-12 05:29:56 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,10 +1,10 @@
 let handler = async(m, { conn, text, participants }) => {
-  let teks = `⋙ *⚠️Admin Group Ngetag⚠️* ⋘
+  let teks = `   ⊙─〔 ıll TᗩGᗩᒪᒪ llı 〕─⊙
 \n *${text ? text : 'Nothing'}*\n\n`
 		      	for (let mem of participants) {
-		            teks += ` @${mem.id.split('@')[0]}\n`
+		            teks += ` ㇱ @${mem.id.split('@')[0]}\n`
 				}
-                teks += `\nbukan elyas`
+                teks += `\n↻ᴋʀɪᴢʏɴ ʙᴏᴛ ᴡʜᴀᴛꜱᴀᴘᴘ↻`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
@@ -13,5 +13,5 @@ handler.command = /^(tagall)$/i
 
 handler.group = true
 handler.admin = true
-
+handler.botadmin = true
 module.exports = handler
