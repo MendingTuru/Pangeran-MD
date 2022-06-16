568793a835e20b747fefb8d5af3d3be863de4359
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-16 11:01:19 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -9,7 +9,7 @@ let anu = `Hallo mypren 👋
 ━━━〔 ıll *Sewa Bot* llı 〕━━㉿
 ━━━━━━━━━━━━━━━━━━━━
 ━━━〔 ıll *HARGA* llı 〕━━㉿
-⬡ *1 BULAN* 5.00
+⬡ *1 BULAN* 5.000
 ⬡ *PERMANEN 2 grup:* 10.000
 ⬡ *PERMANEN 3-6 grup:* 20.000
 ⟩» lebih dari 6 tambah +5.000
