7f557e7a8552e9fc6c6300e99f91ddea27304687
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-16 08:40:15 +0700

    Create info-sc.js

---
+++ plugins/info-sc.js
@@ -0,0 +1,22 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `*––––––『 MAU NGAPAIN OM 』––––––*
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: '🐈 My Istagram', url: sgh}},
+    {index: 2, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 3, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
+    {index: 4, quickReplyButton: {displayText: 'Donasi', id: '.donasi'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: thumb
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['sc']
+handler.tags = ['info']
+handler.command = /^(s(ourcode|c))$/i
+
+export default handler
