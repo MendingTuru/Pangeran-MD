7a263201503d0b0f2f68b18a85dbefddec9e390a
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-16 23:59:30 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -1,11 +1,8 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
-let handler = async (m, { conn }) => {
-let teks = 'DONATE BOT'
-let dana = global.dana
-let pulsa = global.pulsa
-let gopay = global.gopay
-let numberowner = global.numberowner
-let anu = `Hallo mypren 👋
+let handler = async (m) => {
+let krizyn = `
+Hallo mypren 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┏━━━〔 ıll *DONATE* llı 〕━━㉿
 ⬡ *DANA:* ${dana}
@@ -17,17 +14,64 @@ Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ⬡ team yang sudah berdonasi
 ┗━━━━━━━━━━㉿
 Berapapun donasi kalian akan sangat berarti 👍
-
 Terimakasih yang sudah mendonasikan untuk bot
-
 Contact person Owner:
 wa.me/${numberowner} (Owner)
+	`.trim()
+  let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(donasi)).buffer()}, { upload: conn.waUploadToServer }) 
+    const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
+        templateMessage: {
+            hydratedTemplate: {
+                hydratedContentText: krizyn,
+                imageMessage: message.imageMessage, 
+           hydratedFooterText: '📮 Silahkan pilih Command Dibawah ini',
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'GroupBot',
+               url: 'https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv'
+             }
 
-*donasi via follow ig juga boleh*`
-  conn.sendButtonImg(m.chat, fla + teks, anu, instagram, 'Back', '.help', m) 
+           },
+                {
+               urlButton: {
+               displayText: 'Myweb',
+               url: 'https://istagram.com/zalfapontinanak'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'sewa',
+               id: '.sewazifa',
+             }
+             
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Menu',
+               id: '.menu',
+             }
+             
+             },
+               {
+             quickReplyButton: {
+               displayText: 'Runtime',
+               id: '.runtime',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
-handler.help = ['donasi', 'donate']
-handler.tags = ['xp', 'info']
-handler.command = /^(donasi|donate)$/i
 
+handler.tags = ['main', 'info']
+handler.command = /^(donasi|donate)$/i
+handler.help = ['donasi']
 module.exports = handler
