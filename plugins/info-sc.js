13741b0dbbb1de4c4097d9b0900598204a6ef9b4
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-16 22:38:11 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,22 +1,58 @@
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-let text = `*––––––『 MAU NGAPAIN OM 』––––––*
-`
-const templateButtons = [
-    {index: 1, urlButton: {displayText: '🐈 My Istagram 🐈', url: web}},
-    {index: 2, urlButton: {displayText: '🌎 Official Group 🌎', url: gc}},
-    {index: 3, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
-    {index: 4, quickReplyButton: {displayText: 'Donasi', id: '.donasi'}},
-]
-let tm = {
-text: text,
-footer: global.wm,
-templateButtons: templateButtons,
-image: thumb
-}
-conn.sendMessage(m.chat, tm, m)
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let teks = 'SCRIPT'
+let dana = global.dana
+let pulsa = global.pulsa
+let gopay = global.gopay
+let numberowner = global.numberowner
+let anu = `Hallo mypren 👋
+┏━━━〔 ıll *INFO SC* llı 〕━━㉿
+⬡ SC ORI: ıll Hyzer
+⬡ Di RECODE OLEH: ıll Krizyn
+⬡ DI RECODE KEMBALI OLEH: ıll Zifabotz
+┗━━━━━━━━━━━━━━━━━━㉿
+┏━━〔 ıll THANKS TEAM llı 〕━㉿
+⬡ team creator-creator
+⬡ team yang sudah berdonasi
+┗━━━━━━━━━━㉿
+🪀LINK SC VIA GITHUB:
+⤿ https://github.com/Botynyakamu/zmi
+
+🪀LINK SC VIA MEDIAFIRE:
+⤿ Tidak ada
+======••••••••
+
+THANKS YANG SUDAH ADA DI DALAM SC INI :
+____________________
+BIG THANKS TO
+
+⤿ Nurutomo
+    📮 https://github.com/Nurutomo
+⤿ Elyas
+    📮 https://github.com/Paquito1923
+⤿ Hyzer Official
+    📮 https://github.com/Hyzerr
+⤿ Krizyn Ofc
+    📮 https://github.com/krizynofc
+⤿ Zifabotz_offc1
+    📮 https://github.com/Botynyakamu
+┏━━━〔 ıll *DONASI* llı 〕━━㉿
+⬡ *DANA:* ${dana}
+⬡ *GOPAY:* ${gopay}
+⬡ *OVO:* ${pulsa}
+┗━━━━━━━━━━━━━━━━━━㉿
+┏━━〔 ıll *RULES* llı 〕━㉿
+⬡ [❗] *JANGAN DI JUAL BELIKAN*
+⬡ [❗] *KALAU ADA ERROR HUBUNGI TEAM KAMI*
+┗━━━━━━━━━━㉿
+⟩» *jangan lupa donasi kak* «⟨
+Terimakasih yang sudah mendonasikan untuk bot
+
+*Follow Instagram ku juga kak😼*`
+  conn.send2ButtonImg(m.chat, fla + teks, anu, instagram, 'Oke', 'ok', 'Menu', '.menu', m) 
 }
 handler.help = ['sc']
 handler.tags = ['info']
-handler.command = /^(s(ourcode|c))$/i
+handler.command = /^(sc|script)$/i
 
-export default handler
+module.exports = handler
