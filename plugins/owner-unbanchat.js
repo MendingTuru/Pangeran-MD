a3bc26c0aba3e1ce3853dc4f80e289389ba80a72
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-18 02:07:34 +0700

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,4 +1,4 @@
-let handler = async (m, { isOwner, text, isAdmin }) => {
+let handler = async (m, { conn, isOwner, text, isAdmin }) => {
   let who
   if (m.isGroup) {
     if (!(isAdmin || isOwner)) {
@@ -6,7 +6,7 @@ let handler = async (m, { isOwner, text, isAdmin }) => {
       throw false
     }
     if (isOwner) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    who = m.mentionedJid[0] ? m.mentionedJid[0] : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    else who = m.chat
   } else {
     if (!isOwner) {
       global.dfail('owner', m, conn)
@@ -14,10 +14,11 @@ let handler = async (m, { isOwner, text, isAdmin }) => {
     }
     who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
   }
+
   try {
     if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = false
     else global.db.data.users[who].banned = false
-    m.reply(`Done Unban! Bot aktif dichat ${await conn.getName(who) == undefined ? 'ini' : await conn.getName(who)}.`)
+    m.reply(`Berhasil unban! ${await conn.user.name} aktif dichat ${await conn.getName(who) == undefined ? 'ini' : await conn.getName(who)}.`)
   } catch (e) {
     throw `nomor tidak ada didatabase!`
   }
