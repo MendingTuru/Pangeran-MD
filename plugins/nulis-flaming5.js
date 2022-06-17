f14038092926320c46f59680ab4bd113cce30100
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-18 00:46:01 +0700

    Create nulis-flaming5.js

---
+++ plugins/nulis-flaming5.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming5 Zifa&Botcahx'
+  m.reply('_Proses..._')
+  let res = `https://api.zeks.me/api/breakwall?apikey=apivinz&text=${response[0]}`
+  conn.sendFile(m.chat, res, 'gura.jpg', `*SELESAI. . .*`, m, false)
+}
+handler.help = ['flaming5'].map(v => v + ' <text>')
+handler.tags = ['nulis']
+handler.command = /^(flaming5)$/i
+
+module.exports = handler
