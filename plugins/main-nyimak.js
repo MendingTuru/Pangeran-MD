6d0437abbb9f5932b06aacd750e81dbd1c262cbc
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-17 07:21:22 +0700

    Create main-nyimak.js

---
+++ plugins/main-nyimak.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/3e49a66125d766426ee5f.png', m, { packname: "zifabotz", author: "@rozi" })
+}
+
+handler.customPrefix = /^(nyimak|menyimak)$/i
+handler.command = new RegExp
+
+module.exports = handler
