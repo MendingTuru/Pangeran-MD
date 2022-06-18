200a0c574f6f7249fc2529417c61b5f792977dae
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-18 10:23:54 +0700

    Update nulis-flaming5.js

--- plugins/nulis-flaming5.js
+++ plugins/nulis-flaming5.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming5 Zifa&Botcahx'
-  m.reply('_Proses..._')
+  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming5 ROZI'
+  m.reply('_Sedang diproses..._')
   let res = `https://api.zeks.me/api/breakwall?apikey=apivinz&text=${response[0]}`
   conn.sendFile(m.chat, res, 'gura.jpg', `*SELESAI. . .*`, m, false)
 }
 handler.help = ['flaming5'].map(v => v + ' <text>')
-handler.tags = ['nulis']
+handler.tags = ['maker']
 handler.command = /^(flaming5)$/i
 
 module.exports = handler
