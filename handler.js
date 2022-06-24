8178273a123b7b8121792e8236bb8399a2877aae
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-24 09:03:57 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -648,7 +648,6 @@ module.exports = {
             console.error(e)
         } finally {
             //console.log(global.db.data.users[m.sender])
-            await this.sendPresenceUpdate('composing', m.chat)
             let user, stats = global.db.data.stats
             if (m) {
                 if (m.sender && (user = global.db.data.users[m.sender])) {
@@ -710,7 +709,7 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup @user').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang @user!')).replace('@user', '@' + user.split('@')[0])
-                                this.sendButtonImg(id, pp, text, "©️ zifabotz-MD", "AWOKAWOAK👋", "nani", null)
+                                this.sendButtonImg(id, pp, text, "ᯤ by creator bot", "AWOKAWOAK", "bebanjoin", null)
                                 }
                     }
                 }
