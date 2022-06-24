dc91e3dd570aee682e59e3565eb50107c6f615d0
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-24 09:06:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('Multi Device\n Krizyn', {
+CFonts.say('ZIFABOTZ\n by.Rozi', {
   colors: ['blueBright','yellowBright'],                                        font: 'block',
   align: 'center',
 })
-CFonts.say(`SUBSCRIBE CHANNEL HOKEN BEUSZ`, {                                            
+CFonts.say(`SUBSCRIBE CHANNEL YOUTUBE Zifabotz_offc1`, {                                            
 colors: ['yellow'],                                                           
 font: 'console',                                                              align: 'center',
 })
