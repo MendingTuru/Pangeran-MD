1979424fd537fd08df9eacdd866d815a2b172b14
Koko <104513437+kokopangeran@users.noreply.github.com>
2022-06-29 10:21:29 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -7,7 +7,23 @@ handler.before = async function (m) {
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
-   } 
+   }
+
+   if (m.sender.startsWith('91' || '91')) {
+   	global.db.data.users[m.sender].banned = true
+   }
+
+   if (m.sender.startsWith('92' || '92')) {
+   	global.db.data.users[m.sender].banned = true
+   }
+
+   if (m.sender.startsWith('60' || '60')) {
+   	global.db.data.users[m.sender].banned = true
+   }
+
+   if (m.sender.startsWith('44' || '44')) {
+   	global.db.data.users[m.sender].banned = true
+   }
     }
 
 module.exports = handler
