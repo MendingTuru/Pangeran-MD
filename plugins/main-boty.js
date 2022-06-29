088c76fee3868cfe0ef8390ab6ecf87dde2300d6
Koko <104513437+kokopangeran@users.noreply.github.com>
2022-06-29 10:16:48 +0700

    Update main-boty.js

--- plugins/main-boty.js
+++ plugins/main-boty.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/9bc8b84760a433b4d26a3.jpg', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/9bc8b84760a433b4d26a3.jpg', m, { packname: "©ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ-MD×፝֟͜×", author: "@ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×" })
 }
 
 handler.customPrefix = /^(boti|boty|boty binal)$/i
