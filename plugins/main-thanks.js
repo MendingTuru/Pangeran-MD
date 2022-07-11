e42ae09619926ad8677869b84e93ccbbdd23271b
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-12 02:11:44 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let name = await conn.getName(m.sender)
   let wm = global.wm
-krizyn = {
+pangeran = {
     key : {
   remoteJid: 'status@broadcast',
   participant : '0@s.whatsapp.net'
@@ -14,7 +14,7 @@ krizyn = {
         }
        } 
      }
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', krizyn, { packname: "Krizyn Ofc", author: "@mhdfakri_" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', pangeran, { packname: "Koko-MD", author: "@kokopangeran_" })
 }
 
 handler.customPrefix = /^(Terima Kasih|Thanks|tq|makasih)$/i
